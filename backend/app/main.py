745105458d6889a8c7fa382fbaac756e14241170
born71 <born.holiday@hotmail.com>
2025-12-25 15:05:01 +0700

    ini implement login fake

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,39 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 from app.routers import chat
+from pydantic import BaseModel
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 app.include_router(chat.router)
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    return user
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to WebSocket Chat API"}
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
